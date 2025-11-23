f499de1e9ac16ac52a5203c4a75b73092de36bda
Stijn Raeymaekers <9306304+NotCoffee418@users.noreply.github.com>
2025-11-23 03:40:02 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,6 @@ void loop()
 void setupDisplay()
 {
   display.init();
-  display.flipScreenVertically();
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setFont(ArialMT_Plain_10);
   display.clear();
